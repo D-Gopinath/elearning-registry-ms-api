52699f853185f2660f512a19ed9ac63bd54a18b1
D-Gopinath <gopinathd3737@gmail.com>
2022-05-05 17:02:55 +0530

    first commit

---
+++ src/main/java/com/eLearning/ELearningRegistryMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.eLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningRegistryMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningRegistryMsApiApplication.class, args);
+	}
+
+}
